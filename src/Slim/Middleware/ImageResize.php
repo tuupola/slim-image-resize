a7bbcdb66801d1c77f56dc0901ddf410952d9d73
Mika Tuupola <tuupola@appelsiini.net>
2014-04-09 17:56:23 +0300

    Image resizing without caching.

--- src/Slim/Middleware/ImageResize.php
+++ src/Slim/Middleware/ImageResize.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Slim\Middleware;
+use Intervention\Image\Image;
 
 /**
  * Provides automagical image resizing.
@@ -16,7 +17,8 @@ class ImageResize extends \Slim\Middleware {
 
         /* Default options. */
         $this->options = array(
-            "foo" => "bar"
+            "extensions" => array("jpg", "jpeg", "png", "gif"),
+            "cache" => false
         );
 
         if ($options) {
@@ -25,7 +27,38 @@ class ImageResize extends \Slim\Middleware {
     }
 
     public function call() {
-        $request = $this->app->request;
-        $this->next->call();
+        $request  = $this->app->request;
+        $response = $this->app->response;
+
+        $target   = $request->getResourceUri();
+        $pathinfo = pathinfo($target);
+
+        if ($this->shouldResize($pathinfo)) {
+            preg_match("/([^-]+)-(\d*)x(\d*)/", $pathinfo["filename"], $matches);
+
+            $source = $_SERVER["DOCUMENT_ROOT"] . $pathinfo["dirname"] . "/" .
+                      $matches[1] . "." . $pathinfo["extension"];
+
+            $width  = $matches[2] ? $matches[2] : null;
+            $height = $matches[3] ? $matches[3] : null;
+
+            $image = Image::make($source);
+
+            if (null !== $width &&  null !== $height) {
+                $image->grab($width, $height);
+            } else {
+                $image->resize($width, $height, true);
+            }
+
+            $response->header("Content-type", $image->mime);
+            $response->body($image->encode());
+        } else {
+            $this->next->call();
+        }
+    }
+
+    private function shouldResize($pathinfo) {
+        return isset($pathinfo["extension"]) &&
+               in_array($pathinfo["extension"], $this->options["extensions"]);
     }
 }
