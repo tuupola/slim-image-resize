9137724e444017628ae8600eb55447210f2073df
Mika Tuupola <tuupola@appelsiini.net>
2014-10-21 19:37:19 +0300

    Add scale and crop mutator

---
+++ src/Slim/Middleware/ImageResize/ScaleAndCropMutator.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Automagical image resizing from Slim
+ *
+ * Copyright (c) 2013-2014 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-image-resize
+ *
+ */
+
+namespace Slim\Middleware\ImageResize;
+
+class ScaleAndCropMutator extends MutatorAbstract
+{
+    protected static $regexp =
+        "/(?<original>[^-]+)-(?<size>(?<scale>\d*)-(?<width>\d*)x(?<height>\d*))-?(?<signature>[0-9a-z]*)/";
+
+    public function execute()
+    {
+        extract($this->options);
+
+        /* Scale with the given percentage... */
+        $scaled_width  = round($scale / 100 * $this->image->width());
+        $scaled_height = round($scale / 100 * $this->image->height());
+        $this->image->resize($scaled_width, $scaled_height);
+
+        $width = $width ? $width : $scaled_width;
+        $height = $height ? $height : $scaled_height;
+
+        /* ... and crop. */
+        $this->image->crop($width, $height);
+
+        return $this;
+    }
+}
---
+++ test/ScaleAndCropMutatorTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Automagical image resizing from Slim
+ *
+ * Copyright (c) 2013-2014 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-image-resize
+ *
+ */
+
+namespace Test;
+
+use Slim\Middleware\ImageResize;
+use Slim\Middleware\ImageResize\ScaleAndCropMutator;
+
+class ScaleAndCropMutatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testShouldBeTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testShouldParseDimensions()
+    {
+        $_SERVER["DOCUMENT_ROOT"] = "/var/www/www.example.com/public";
+
+        $mutator = new ScaleAndCropMutator();
+        $parsed = $mutator->parse("images/viper-70-400x200.jpg");
+        $this->assertEquals($parsed["filename"], "viper-70-400x200");
+        $this->assertEquals($parsed["basename"], "viper-70-400x200.jpg");
+        $this->assertEquals($parsed["extension"], "jpg");
+        $this->assertEquals($parsed["dirname"], "images");
+        $this->assertEquals($parsed["original"], "viper");
+        $this->assertEquals($parsed["size"], "70-400x200");
+        $this->assertEquals($parsed["width"], "400");
+        $this->assertEquals($parsed["height"], "200");
+        $this->assertEquals($parsed["scale"], "70");
+        $this->assertEquals($parsed["source"], "/var/www/www.example.com/public/images/viper.jpg");
+        //$this->assertEquals($parsed["cache"], "/var/www/www.example.com/public/cache/images/viper-400x200.jpg");
+        $this->assertNull($parsed["signature"]);
+    }
+
+    public function testParseShouldReturnFalse()
+    {
+        $middleware = new ImageResize();
+        $parsed = $middleware->mutator->parse("images/viper-new.jpg");
+        $this->assertFalse($parsed);
+    }
+}
